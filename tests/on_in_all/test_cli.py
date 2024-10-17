6f15f6fb58969577bb73b0bc3dcf522287b4f815
tianzhichao <tyronextian@gmail.com>
2024-10-17 17:35:47 +0800

    [FEATURE] httx 调用大模型端

--- tests/on_in_all/test_cli.py
+++ tests/on_in_all/test_cli.py
@@ -11,10 +11,11 @@ from components.one_in_all.cli import LLMClientAPI
 
 
 class TestCli(unittest.TestCase):
+    api_base = "https://xiaoai.plus/v1"
+    api_key = "sZoc7LZQQ9kRyDqHD4Ca700e6f81487fBe608aEc0650D066"
+
     def test_chat(self):
-        api_base = "http://10.141.103.6:3000/v1"
-        api_key = "sk-1v8PLLXPZbpvi4yx74A3Dc535231496bB2B77f83453bAb9a"
-        llm_cli = LLMClientAPI(api_key=api_key, api_base=api_base)
+        llm_cli = LLMClientAPI(api_key=self.api_key, api_base=self.api_base)
 
         test_answer = "你好"
         async def run_chat():
@@ -32,12 +33,9 @@ class TestCli(unittest.TestCase):
         asyncio.run(run_chat())
 
     def test_chat_stream(self):
-        api_base = "http://10.141.103.6:3000/v1"
-        api_key = "sk-1v8PLLXPZbpvi4yx74A3Dc535231496bB2B77f83453bAb9a"
-        llm_cli = LLMClientAPI(api_key=api_key, api_base=api_base)
+        llm_cli = LLMClientAPI(api_key=self.api_key, api_base=self.api_base)
 
         test_answer = "你好"
-
         async def run_chat():
             async for item in llm_cli.chat(model="qwen2-72b",
                                            messages=[{"role": "system",

c2b0eefa9b73e00496dc8412e347e02d76930770
tianzhichao <tyronextian@gmail.com>
2024-10-18 13:43:01 +0800

    [FEATURE] setup file

--- setup.py
+++ setup.py
@@ -8,23 +8,24 @@ Time: 2024/10/17 13:15
 from setuptools import setup, find_packages
 
 setup(
-    name='my_component_lib',              # 项目名称
-    version='0.1.0',                      # 版本号
-    author='你的名字',                      # 作者
-    author_email='你的邮箱@example.com',  # 作者邮箱
-    description='A description of your component library',  # 项目简要描述
-    long_description=open('README.md').read(),  # 长描述（通常是README文件内容）
-    long_description_content_type='text/markdown',  # 长描述内容类型
-    url='https://github.com/yourusername/my_component_lib',  # 项目主页
-    packages=find_packages(),              # 自动发现包
-    install_requires=[                     # 依赖包列表
-        'requests>=2.25.1',
-        'numpy>=1.19.0',
+    name='llb_lib',
+    version='0.1.0',
+    author='tyrone',
+    author_email='tyronextian@gmail.com',
+    description='大模型调用通用框架搭建',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tyronemaxi/llm_lib',
+    packages=find_packages(),
+    install_requires=[
+        "loguru == 0.7.0",
+        "httpx == 0.27.2",
+        "ujson == 5.10.0",
     ],
-    classifiers=[                          # 分类信息
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',              # Python 版本要求
+    python_requires='>=3.8',
 )
